2bcc68844a8586feb94b4556f5322ea3f0abfae2
K7IT2024 <dhanyavani1991@gmail.com>
2026-07-15 13:05:14 +0530

    Adding Login validations

--- src/main/java/com/foodrescue/dto/RegisterRequest.java
+++ src/main/java/com/foodrescue/dto/RegisterRequest.java
@@ -1,14 +1,28 @@
 package com.foodrescue.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class RegisterRequest {
 
+    @NotBlank(message = "Full name is required")
     private String fullName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     private String email;
+
+    @NotBlank(message = "Mobile number is required")
     private String mobileNumber;
+
     private String location;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
+
     private String role;
 }
